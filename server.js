e81cd2ff0432616c3c5c63ed4d37fd165c644a05
Sanjay-V-Nair <sanjay.v2021@vitbhopal.ac.in>
2023-09-22 22:57:58 +0530

    json format added in the api request

--- server.js
+++ server.js
@@ -6,9 +6,15 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+//If not needed in JSON file format
+// app.get('/api/contacts', (req, res) => {
+//     res.send("Get all contacts")
+// })
+
 app.get('/api/contacts', (req, res) => {
-    res.send("Get all contacts")
+    res.status(200).json({ message: "Get all contacts" })
 })
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
